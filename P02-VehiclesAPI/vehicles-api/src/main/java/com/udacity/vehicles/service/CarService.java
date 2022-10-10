661967cb70b26d54f432740775d7edaa483555a8
Mohamed Eldefrawy <mohamed.ahmed.aly.d@gmail.com>
2022-10-10 08:13:48 +0200

    update car API controller

--- P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ P02-VehiclesAPI/vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -23,10 +23,6 @@ public class CarService {
 
     public CarService(CarRepository repository, WebClient maps, WebClient pricing, PricingService pricingService, MapService mapService) {
 
-        /**
-         * TODO: Add the Maps and Pricing Web Clients you create
-         *   in `VehiclesApiApplication` as arguments and set them here.
-         */
         this.repository = repository;
         this.maps = maps;
         this.pricing = pricing;
@@ -50,35 +46,12 @@ public class CarService {
      * @return the requested car's information, including location and price
      */
     public Car findById(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         *   Remove the below code as part of your implementation.
-         */
-
         var selectedCar = this.repository.findById(id);
         if (!selectedCar.isPresent()) {
             throw new CarNotFoundException("Car Not Found");
         }
-        /**
-         * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
-         *   to get the price based on the `id` input'
-         * TODO: Set the price of the car
-         * Note: The car class file uses @transient, meaning you will need to call
-         *   the pricing service each time to get the price.
-         */
 
         var price = this.pricingService.getPrice(id);
-
-        /**
-         * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
-         *   to get the address for the vehicle. You should access the location
-         *   from the car object and feed it to the Maps service.
-         * TODO: Set the location of the vehicle, including the address information
-         * Note: The Location class file also uses @transient for the address,
-         * meaning the Maps service needs to be called each time for the address.
-         */
-
 
         var selectedCarLocation = selectedCar.get().getLocation();
         var location = this.mapService.getAddress(selectedCarLocation);
@@ -111,20 +84,12 @@ public class CarService {
      * @param id the ID number of the car to delete
      */
     public void delete(Long id) {
-        /**
-         * TODO: Find the car by ID from the `repository` if it exists.
-         *   If it does not exist, throw a CarNotFoundException
-         */
 
         var selectedCar = this.repository.findById(id);
         if (!selectedCar.isPresent()) {
             throw new CarNotFoundException("Car Not Found");
         }
 
-
-        /**
-         * TODO: Delete the car from the repository.
-         */
         this.repository.delete(selectedCar.get());
     }
 }
