af047432095c422b6b46717b3481a809a23ef2ce
Mohamed ElDafrawy <mohamed.eldafrawy@vodafone.com>
2022-10-11 22:06:12 +0200

    Create unit test for pricing service

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceIntegrationTest.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceIntegrationTest.java
@@ -2,17 +2,16 @@ package com.udacity.pricing;
 
 import com.udacity.pricing.domain.price.Price;
 import org.junit.Test;
-import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.math.BigDecimal;
@@ -22,7 +21,6 @@ import static org.hamcrest.Matchers.equalTo;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureMockMvc
 public class PricingServiceIntegrationTest {
 
     @Autowired
@@ -32,23 +30,9 @@ public class PricingServiceIntegrationTest {
     private Long port;
     private final String url = "http://localhost:";
 
-
     @Test
-    public void lastGetValidPrice() {
-        ResponseEntity<Price> response =
-                this.restTemplate.exchange(url + port + "/prices/1", HttpMethod.GET, ResponseEntity.EMPTY, Price.class);
-        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
-    }
-
-    @Test
-    public void getInvalidPrice() {
-        ResponseEntity<Price> response =
-                this.restTemplate.getForEntity(url + port + "/prices/123", Price.class);
-        assertThat(response.getStatusCode(), equalTo(HttpStatus.NOT_FOUND));
-    }
-
-    @Test
-    public void firstInsertVehiclePrice() {
+    public void testPriceApiCrudOperations() {
+        // First create price
         var newPrice = new Price();
         newPrice.setVehicleId(1L);
         newPrice.setCurrency("EGP");
@@ -56,7 +40,21 @@ public class PricingServiceIntegrationTest {
         var httpEntity = new HttpEntity<Price>(newPrice);
         var response = this.restTemplate.exchange(url + port + "/prices", HttpMethod.POST, httpEntity, Price.class);
         assertThat(response.getStatusCode(), equalTo(HttpStatus.CREATED));
-    }
 
+        // get by id
+        response =
+                this.restTemplate.getForEntity(url + port + "/prices/1", Price.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+
+        // update
+        newPrice.setCurrency("USD");
+        httpEntity = new HttpEntity<>(newPrice);
+        this.restTemplate.exchange(url + port + "/prices", HttpMethod.PUT, httpEntity, Price.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+
+        //delete
+        this.restTemplate.exchange(url + port + "/prices/1", HttpMethod.DELETE, HttpEntity.EMPTY, Void.class);
+        assertThat(response.getStatusCode(), equalTo(HttpStatus.OK));
+    }
 
 }
